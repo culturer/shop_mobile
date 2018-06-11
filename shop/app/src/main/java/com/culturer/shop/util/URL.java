17dfa237d1f98fa36ab65e9ead1817c2a1dd073a
cultuler <78901214@qq.com>
2018-06-11 08:53:12 +0800

    增加OSS依赖

--- shop/app/src/main/java/com/culturer/shop/util/URL.java
+++ shop/app/src/main/java/com/culturer/shop/util/URL.java
@@ -1,6 +1,7 @@
 package com.culturer.shop.util;
 
 public class URL {
-	public static final String HOST = "http://192.168.43.146:8080/";
+	public static final String HOST = "http://192.168.0.108:8080/";
+//	public static final String HOST = "http://192.168.43.146:8080/";
 //	public static final String HOST = "http://120.78.153.27:8080/";
 }
